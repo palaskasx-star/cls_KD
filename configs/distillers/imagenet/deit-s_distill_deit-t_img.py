cc1f3e392c1ee537596fc8236c1cdad6138917c6
palaskasc <palaskasx@gmail.com>
2026-02-03 13:49:21 +0200

    Update deit-s_distill_deit-t_img.py

--- configs/distillers/imagenet/deit-s_distill_deit-t_img.py
+++ configs/distillers/imagenet/deit-s_distill_deit-t_img.py
@@ -1,74 +1,88 @@
-_base_ = [
-    '../../deit/deit-tiny_pt-4xb256_in1k.py'
-]
-# model settings
-find_unused_parameters = False
+    _base_ = [
+        '../../deit/deit-tiny_pt-4xb256_in1k.py'
+    ]
+    # model settings
+    find_unused_parameters = False
 
-# distillation settings
-use_logit = True
-is_vit = True
+    # distillation settings
+    use_logit = True
+    is_vit = True
 
-# config settings
-wsld = False
-dkd = False
-kd = False
-nkd = True
-vitkd = True
+    # config settings
+    wsld = False
+    dkd = False
+    kd = False
+    nkd = True
+    vitkd = True
+    mf = True
 
-# method details
-model = dict(
-    _delete_ = True,
-    type='ClassificationDistiller',
-    use_logit = use_logit,
-    is_vit = is_vit,
-    teacher_pretrained = 'https://download.openmmlab.com/mmclassification/v0/deit/deit-small_pt-4xb256_in1k_20220218-9425b9bb.pth',
-    teacher_cfg = 'configs/deit/deit-small_pt-4xb256_in1k.py',
-    student_cfg = 'configs/deit/deit-tiny_pt-4xb256_in1k.py',
-    distill_cfg = [ dict(methods=[dict(type='ViTKDLoss',
-                                       name='loss_vitkd',
-                                       use_this = vitkd,
-                                       student_dims = 192,
-                                       teacher_dims = 384,
-                                       alpha_vitkd=0.00003,
-                                       beta_vitkd=0.000003,
-                                       lambda_vitkd=0.5,
-                                       )
+    # method details
+    model = dict(
+        _delete_ = True,
+        type='ClassificationDistiller',
+        use_logit = use_logit,
+        is_vit = is_vit,
+        teacher_pretrained = 'https://download.openmmlab.com/mmclassification/v0/deit3/deit3-small-p16_in21k-pre_3rdparty_in1k_20221009-dcd90827.pth',
+        teacher_cfg = 'configs/deit3/deit3-small-p16_64xb64_in1k.py',
+        student_cfg = 'configs/deit/deit-tiny_pt-4xb256_in1k.py',
+        distill_cfg = [ dict(methods=[dict(type='ViTKDLoss',
+                                        name='loss_vitkd',
+                                        use_this = vitkd,
+                                        student_dims = 192,
+                                        teacher_dims = 384,
+                                        alpha_vitkd=0.00003,
+                                        beta_vitkd=0.000003,
+                                        lambda_vitkd=0.5,
+                                        )
+                                    ]
+                            ),
+                        dict(methods=[dict(type='WSLDLoss',
+                                        name='loss_wsld',
+                                        use_this = wsld,
+                                        temp=2.0,
+                                        alpha=2.5,
+                                        num_classes=1000,
+                                        )
+                                    ]
+                            ),
+                        dict(methods=[dict(type='DKDLoss',
+                                        name='loss_dkd',
+                                        use_this = dkd,
+                                        temp=1.0,
+                                        alpha=1.0,
+                                        beta=0.5,
+                                        )
+                                    ]
+                            ),
+                        dict(methods=[dict(type='NKDLoss',
+                                        name='loss_nkd',
+                                        use_this = nkd,
+                                        temp=1.0,
+                                        gamma=1.0,
+                                        )
+                                    ]
+                            ),
+                        dict(methods=[dict(type='KDLoss',
+                                        name='loss_kd',
+                                        use_this = kd,
+                                        temp=1.0,
+                                        alpha=0.5,
+                                        )
+                                    ]
+                            ),
+                        dict(methods=[dict(type='MFLoss',
+                                        name='loss_mf',
+                                        use_this = mf,
+                                        K=6304,
+                                        temperature=0.1,
+                                        sinkhorn_iters=3,
+                                        normalize_input=True,
+                                        weight_mf=1.0,
+                                        weight_koleo_data=0.0,
+                                        weight_koleo_proto=0.0,
+                                        )
                                 ]
-                        ),
-                    dict(methods=[dict(type='WSLDLoss',
-                                       name='loss_wsld',
-                                       use_this = wsld,
-                                       temp=2.0,
-                                       alpha=2.5,
-                                       num_classes=1000,
-                                       )
-                                ]
-                        ),
-                    dict(methods=[dict(type='DKDLoss',
-                                       name='loss_dkd',
-                                       use_this = dkd,
-                                       temp=1.0,
-                                       alpha=1.0,
-                                       beta=0.5,
-                                       )
-                                ]
-                        ),
-                    dict(methods=[dict(type='NKDLoss',
-                                       name='loss_nkd',
-                                       use_this = nkd,
-                                       temp=1.0,
-                                       gamma=1.0,
-                                       )
-                                ]
-                        ),
-                    dict(methods=[dict(type='KDLoss',
-                                       name='loss_kd',
-                                       use_this = kd,
-                                       temp=1.0,
-                                       alpha=0.5,
-                                       )
-                                ]
-                        ),
+                            ),
 
-                   ]
-    )
+                    ]
+        )
