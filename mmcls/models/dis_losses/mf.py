3f8335d34e6d059d6686fa41401b497c0ba234a9
palaskasc <palaskasx@gmail.com>
2026-02-05 19:49:12 +0200

    Update mf.py

--- mmcls/models/dis_losses/mf.py
+++ mmcls/models/dis_losses/mf.py
@@ -209,9 +209,9 @@ class MFLoss(nn.Module):
             l_mf = (loss12 + loss21) / 2
 
             # Aggregate
-            total_loss_mf += l_mf
-            total_loss_koleo_d += l_koleo_d
-            total_loss_koleo_p += l_koleo_p
+            total_loss_mf += l_mf/3
+            total_loss_koleo_d += l_koleo_d/3
+            total_loss_koleo_p += l_koleo_p/3
 
         # Final weighted sum
         final_loss = (self.weight_mf * total_loss_mf + 
